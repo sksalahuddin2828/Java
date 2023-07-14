82c81126a912caae2b26d0a21463b466ee565c29
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:03:14 +0600

    Update Coding_Challenges.java

--- Coding_Challenges.java
+++ Coding_Challenges.java
@@ -14,4 +14,33 @@
 
 // 1. Converting Decimal Numbers to Octal Numbers:
 
+public class Main {
+    public static void main(String[] args) {
+        int decimal_number = 25;
+        int[] octal_number = new int[100];
+        int i = 0;
+
+        while (decimal_number > 0) {
+            octal_number[i] = decimal_number % 8;
+            decimal_number = decimal_number / 8;
+            i++;
+        }
+
+        System.out.print("Octal number: ");
+
+        for (int j = i - 1; j >= 0; j--) {
+            System.out.print(octal_number[j]);
+        }
+
+        return;
+    }
+}
+
 //----------------------------------------------------------------------------------------------------------------------------------------------
+
+// 2. Reversing an Integer:
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// 3. 
