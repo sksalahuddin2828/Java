7ef1344ce8b449b0cf988a85a600d833b22d2edd
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:04:32 +0600

    Update Coding_Challenges.java

--- Coding_Challenges.java
+++ Coding_Challenges.java
@@ -57,4 +57,31 @@ public class Main {
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
-// 3. 
+// 3. Printing the Fibonacci Series using Recursion:
+
+public class Main {
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.print("Fibonacci series: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        return;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+// 4. Returning the Nth Value from the Fibonacci Sequence:
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
