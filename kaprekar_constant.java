93b7369a7dec20f39c9effc9c4acdab236bd18e3
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:09:26 +0600

    Update kaprekar_constant.java

--- kaprekar_constant.java
+++ kaprekar_constant.java
@@ -30,3 +30,32 @@ public class KaprekarConstant {
         System.out.println("Number of steps to reach Kaprekar constant: " + steps);
     }
 }
+
+//-------------------------------------------------------------------------------------------------
+
+public class KaprekarConstant {
+    public static int kaprekarConstant(int n) {
+        int count = 0;
+        while (n != 6174) {
+            count++;
+            String digits = String.format("%04d", n);
+            char[] digitsArray = digits.toCharArray();
+            Arrays.sort(digitsArray);
+            int ascending = Integer.parseInt(new String(digitsArray));
+            for (int i = 0, j = 3; i < j; i++, j--) {
+                char temp = digitsArray[i];
+                digitsArray[i] = digitsArray[j];
+                digitsArray[j] = temp;
+            }
+            int descending = Integer.parseInt(new String(digitsArray));
+            n = descending - ascending;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(kaprekarConstant(1234));  
+    }
+}
+
+// Output: 3
