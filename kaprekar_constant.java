85739cdd41b10c32f2a61dfe36fa7dd6e6a1fa7c
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:31:04 +0600

    Create kaprekar_constant.java

---
+++ kaprekar_constant.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class KaprekarConstant {
+    public static int kaprekarConstant(int n) {
+        int count = 0;
+        while (n != 6174) {
+            count++;
+            String digits = String.format("%04d", n);
+            char[] digitsArray = digits.toCharArray();
+            Arrays.sort(digitsArray);
+            int ascending = Integer.parseInt(new String(digitsArray));
+            for (int i = 0, j = 3; i < j; i++, j--) {
+                char temp = digitsArray[i];
+                digitsArray[i] = digitsArray[j];
+                digitsArray[j] = temp;
+            }
+            int descending = Integer.parseInt(new String(digitsArray));
+            n = descending - ascending;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        int steps = kaprekarConstant(num);
+        System.out.println("Number of steps to reach Kaprekar constant: " + steps);
+    }
+}
